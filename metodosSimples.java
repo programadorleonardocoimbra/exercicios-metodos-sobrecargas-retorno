6579a669c71379175bc6257eb720241e073a0aaf
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-20 13:48:37 -0300

    inicio

---
+++ metodosSimples.java
@@ -0,0 +1,9 @@
+public class metodosSimples {
+    public static void saudacao() {
+        System.out.println("Olá, mundo!");
+    }
+
+    public static void main(String[] args) {
+        saudacao();
+    }
+}
